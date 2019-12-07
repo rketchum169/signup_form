4b2510d412a8e1f6fee6d2e57ea97de791a01336
Ryan Ketchum <ryan@PAMAC6967.local>
2019-12-07 14:07:38 -0800

    other tests have been started

--- process.php
+++ process.php
@@ -6,12 +6,46 @@ function check($username) {
 	$length = strlen($username);
 	if ($length < 2) {
 		echo "Please enter a username longer than 2 characters";
-	} else {
-		echo "You got it!";
+	} else { 
+		echo "you got it!";
 	}
 }
 
+function email($email) {
+	$symbol = "@";
+	if (strpos($email, $symbol) !== false) {
+		echo "Yay!";
+	} else {
+		echo "This is not an email";
+	}
+}
+
+function age($age) {
+ $sep = substr($age, 0, 3);
+ $year = (int)$sep;
+ if ((2019-$year)<18) {
+ echo "You're too young";
+ } else {
+ echo "Your're old Welcome";
+ }
+ }
+
+function age($age) {
+ $sep = substr($age, 0, 3);
+ $year = (int)$sep;
+ if ((2019-$year)<18) {
+ echo "You're too young";
+  } else {
+  echo "Your're old Welcome";
+  }
+  }
+
+
+
 check($_POST["username"]);
+email($_POST["email"]);
+age($_POST["birthday"]);
+password($_POST["password"],$_POST["password2"]);
 
 ?>
 <body>
