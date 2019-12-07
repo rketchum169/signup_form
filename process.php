aea8b4c3cbb4750f298fabe7e9e353f31a56b22d
Ryan Ketchum <ryan@PAMAC6967.local>
2019-12-07 13:28:44 -0800

    Got the first check

--- process.php
+++ process.php
@@ -2,24 +2,16 @@
 
 <?php
 
-	$username = $_POST["username"];
-	$email = $_POST["email"];
-	$first_name = $_POST["first_name"];
-	$last_name = $_POST["last_name"];
-	$password = $_POST["password"];
-	$password2 = $_POST["password2"];
-	$bday = $_POST["birthday"];
-
-function check() {
-	$length = (int)$username;
-	if ($length <= 2) {
+function check($username) {
+	$length = strlen($username);
+	if ($length < 2) {
 		echo "Please enter a username longer than 2 characters";
 	} else {
 		echo "You got it!";
 	}
 }
 
-check();
+check($_POST["username"]);
 
 ?>
 <body>
