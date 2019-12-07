8ab5b55b703b63243212a710e05e658b053777e0
Ryan Ketchum <ryan@PAMAC6967.local>
2019-12-07 12:13:07 -0800

    Started working on the form

---
+++ signup.php
@@ -0,0 +1,32 @@
+<html>
+	<body>
+	  <h1>Secret Page</h1>
+		<hr>
+   <form action="/process.php" method="POST">
+		<h2>Signup</h2>
+		<p> Please fill out the fields below </p>
+		<hr>
+    <label>Username:</label>
+		<input type="text" value="" name="username" />
+    <br>
+    <label>Email:</label>
+    <input type="email" value="" name="email" />
+    <br>
+    <label>First Name:</label>
+    <input type="text" value="" name="first_name" />
+    <br>
+    <label>Last Name:</label>
+    <input type="text" value="" name="last_name" />
+		<br>
+		<label>Password:</label>
+    <input type="password" value="" name="password" />
+		<br>
+		<label>Confirm Password:</label>
+		<input type="password" value="" name="password2" />
+		<br>
+		<label>Birthday:</label>
+		<input type="date" value="" name="birthday" />
+		</form>
+	</body>
+</html>
+
